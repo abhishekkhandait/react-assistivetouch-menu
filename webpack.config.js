852a06b16327f2e6dfa66f0435394824a16c727e
Abhishek Khandait <khandaitabhishek@gmail.com>
2019-06-13 00:17:52 +0530

    fix: moved react react-dom to externals

--- webpack.config.js
+++ webpack.config.js
@@ -6,46 +6,58 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "./dist"),
 		filename: "index.js",
-		libraryTarget: 'commonjs2'
+		libraryTarget: 'umd',
+		library: 'ReactAssistivetouchMenu'
 	},
 	module: {
 		rules: [
 			{
-				test: /\.tsx?$/,
-				loader: "ts-loader",
-				exclude: /node_modules/
+				test: /\.ts(x|)?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "ts-loader",
+				},
 			},
 			{
 				test: /\.css$/,
-				use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-			},
+				use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+				exclude: /node_modules/,
+			}
 		]
 	},
 	resolve: {
-		extensions: [".ts", ".tsx", ".js", ".jsx"]
+		extensions: [".ts", ".tsx", ".js", ".jsx"],
+		alias: {
+			"react": path.resolve(__dirname, "./node_modules/react"),
+			"react-dom": path.resolve(__dirname, "./node_modules/react-dom")
+		}
 	},
 	performance: {
 		hints: false
 	},
-	devtool: "#eval-source-map"
-};
-
-if (process.env.NODE_ENV === "production") {
-	module.exports.devtool = "#source-map";
-	module.exports.plugins = (module.exports.plugins || []).concat([
+	devtool: "none",
+	externals: {
+		"react": {
+			commonjs: "react",
+			commonjs2: "react",
+			amd: "react",
+			root: "React"
+		},
+		"react-dom": {
+			commonjs: "react-dom",
+			commonjs2: "react-dom",
+			amd: "react-dom",
+			root: "ReactDOM"
+		}
+	},
+	plugins: [
 		new webpack.DefinePlugin({
 			"process.env": {
-				NODE_ENV: '"production"'
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			sourceMap: true,
-			compress: {
-				warnings: false
+				NODE_ENV: JSON.stringify('production'),
 			}
 		}),
 		new webpack.LoaderOptionsPlugin({
 			minimize: true
 		})
-	]);
-}
+	]
+};
